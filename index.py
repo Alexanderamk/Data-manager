3386710edf107f4320ac09325e6d070f0298e33a
Alexander <alexanderamk71@gmail.com>
2024-12-07 14:26:36 +0630

    Final version with file input

--- index.py
+++ index.py
@@ -4,17 +4,25 @@ from main_menu import menu
 
 
 def main():
-    file_location = "files/blank.csv"
-    headers, dict_datas, has_data = reading_data.open_file(file_location)
+    file_location = input("File location: ")
+    located = reading_data.open_file(file_location)[0]
+
+    if located:
+        headers, dict_datas, has_data = reading_data.open_file(file_location)[1:]
+    else:
+        print("File is not found!")
+        return
+
     if not headers:
         print("File must have at least a header.")
         return
+    
     file = file_obj.FileMaintaining(headers, dict_datas)
 
     menu()
     mode = ""
     while mode != "q":
-        mode = input("mode: ").lower()
+        mode = input("mode (-h for help): ").lower()
         if mode == "dp":
             if has_data:
                 file.display()
@@ -34,13 +42,15 @@ def main():
             file.delete()
         elif mode == "up":
             file.update()
+        elif mode == "cls":
+            file.clear_screen()
         elif mode == "q":
             return
         elif mode == "save":
-            reading_data.saving_file(file.dict_datas, headers)
-        else:
+            reading_data.saving_file(file.dict_datas, headers)      
+        elif mode == "-h":
             menu()
-
+    
 
 if __name__ == "__main__":
     main()
--- main_menu.py
+++ main_menu.py
@@ -1,12 +1,13 @@
 def menu():
     messages = [
         ["'dp' to display the data    "],
-        ["'a' to add new data         "],
         ["'s' to search data          "],
+        ["'a' to add new data         "],
         ["'del' to delete the data    "],
         ["'up' to update/edit the data"],
+        ["'cls' to clear the screen"   ],
         ["'q' to quit the program     "],
-        ["'save' to save the file"]
+        ["'save' to save the file     "]
     ]
 
     for message in messages:
